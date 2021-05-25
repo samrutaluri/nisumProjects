f10259457e06c86a4c75fbedbbaa048557d05692
samrutaluri <84303589+samrutaluri@users.noreply.github.com>
2021-05-25 09:10:49 +0530

    Add files via upload

---
+++ Collections/src/main/java/com/nisum/collection/Set.java
@@ -0,0 +1,20 @@
+package com.nisum.collection;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+class Set {
+	public static void main(String args[]) {
+		// Creating HashSet and adding elements
+		HashSet<String> set = new HashSet();
+		set.add("One");
+		set.add("Two");
+		set.add("Three");
+		set.add("Four");
+		set.add("Five");
+		Iterator<String> i = set.iterator();
+		while (i.hasNext()) {
+			System.out.println(i.next());
+		}
+	}
+}
---
+++ Collections/src/test/java/com/nisum/collection/CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
