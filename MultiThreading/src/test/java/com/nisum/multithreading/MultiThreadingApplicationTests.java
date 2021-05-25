90843fdf05213976c0fd69e7d9263f528d482f0c
samrutaluri <84303589+samrutaluri@users.noreply.github.com>
2021-05-25 09:05:35 +0530

    Add files via upload

---
+++ MultiThreading/src/test/java/com/nisum/multithreading/MultiThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
