68ab2f11ad09e912eddbb13a254c3c4a4b48dcf2
samrutaluri <84303589+samrutaluri@users.noreply.github.com>
2021-05-25 09:11:27 +0530

    Add files via upload

---
+++ OopConcepts/src/main/java/com/example/demo/Inheritance.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+abstract class Car {
+	abstract void accelerate();
+}
+
+class Suzuki extends Car {
+	void accelerate() {
+		System.out.println("Suzuki::accelerate");
+
+	}
+}
+
+class Main {
+	public static void main(String args[]) {
+		Car obj = new Suzuki();
+		obj.accelerate();
+	}
+}
