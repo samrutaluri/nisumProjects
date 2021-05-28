2d800e04636b1ea60130710cb7c5f557d6cf1f6f
samrutaluri <84303589+samrutaluri@users.noreply.github.com>
2021-05-28 09:48:47 +0530

    Add files via upload

---
+++ PanRegex/src/main/java/com/nisum/regex/PanRegexApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanRegexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanRegexApplication.class, args);
+	}
+
+}
---
+++ PanRegex/src/test/java/com/nisum/regex/PanRegexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.regex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanRegexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
